254fc3abcd507d1c147a059cdec2f6f6ecb26850
Neiloe <florent.feugere@gmail.com>
2021-10-15 16:59:31 +0200

    précisions

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,6 +2,9 @@
     <div>
         <Calendar />
     </div>
+    <p>
+        L'idée ici est de réaliser le calendrier from scratch et de faire en sorte que chaque projet que je réalise soit visible sur le calendrier.
+    </p>
 </template>
 
 <script>
